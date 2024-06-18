a032a0cee98853ecf24db7a47a72d475ccb7475a
jordon266 <jordon.j.murray@torontomu.ca>
2024-06-17 21:39:26 -0400

    fixed render deprecation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>React App</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode>
+  <h1>React App</h1>
+</React.StrictMode>);
